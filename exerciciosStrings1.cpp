f71c193c6a7027407e55a144d3a3dba6f0de316a
VitorNoe <162385647+VitorNoe@users.noreply.github.com>
2024-04-12 22:10:33 +0000

    Strings e arrays

---
+++ exerciciosStrings1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int palavra
+    
+    std::string palavra;
+
+    printf("Digite uma palavra: ");
+    std::scanf(std::scanf, palavra);
+
+    int tamanho = &palavra();
+
+    printf("O tamanho da palavra digitada é: %d\n", tamanho);
+
+    return 0;
+}
